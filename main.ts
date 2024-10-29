6e474f266c0e0a06660d5b1f3e694f98f7043d9f
Noxid86 <j.dixon.meta@gmail.com>
2024-10-29 13:44:46 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,174 @@
+namespace SpriteKind {
+    export const hoe = SpriteKind.create()
+    export const seedBag = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (equippedTool == "hoe") {
+        if (tiles.tileAtLocationEquals(Farmer.tilemapLocation(), sprites.castle.tileGrass1)) {
+            tiles.setTileAt(Farmer.tilemapLocation(), assets.tile`myTile2`)
+        }
+    } else if (equippedTool == "seedBag") {
+        if (tiles.tileAtLocationEquals(Farmer.tilemapLocation(), assets.tile`myTile2`)) {
+            tiles.setTileAt(Farmer.tilemapLocation(), assets.tile`myTile0`)
+        }
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.seedBag, function (sprite, otherSprite) {
+    equippedTool = "seedBag"
+    EquippedItem.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 6 . 6 . 6 . 6 . . . . . 
+        . . . . . 6 6 6 6 6 . . . . . . 
+        . . . . . e e e e e . . . . . . 
+        . . . . . 6 6 6 6 6 . . . . . . 
+        . . . . 6 6 6 6 6 6 6 . . . . . 
+        . . . 6 6 6 6 6 6 6 6 6 . . . . 
+        . . . 6 6 6 6 6 6 6 6 6 . . . . 
+        . . . 6 6 6 6 6 6 6 6 6 . . . . 
+        . . . . 6 6 6 6 6 6 6 . . . . . 
+        . . . . . 6 6 6 6 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.hoe, function (sprite, otherSprite) {
+    equippedTool = "hoe"
+    EquippedItem.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 9 9 9 9 9 . . . . . . 
+        . . . . . . . . e . . . . . . . 
+        . . . . . . . . e . . . . . . . 
+        . . . . . . . . e . . . . . . . 
+        . . . . . . . . e . . . . . . . 
+        . . . . . . . . e . . . . . . . 
+        . . . . . . . . e . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+})
+let equippedTool = ""
+let EquippedItem: Sprite = null
+let Farmer: Sprite = null
+Farmer = sprites.create(img`
+    ...bbccccccbb...
+    ..bdddddddd1db..
+    .bddbbbbbbbbddb.
+    .cdb11111111bdc.
+    .cbcbbbbbbbbcbc.
+    .fbbd111111dbbf.
+    .fcd11111111dcf.
+    f6cdd111111ddc6f
+    f66cbbbbbbbbcc6f
+    fcbb33333333bbcf
+    fbb3333333333bbf
+    fbb3d111111d3bbf
+    fbd1111111111dbf
+    fdd1111111111ddf
+    fdd1111111111ddf
+    fdd1111111111ddf
+    fdd1111111111ddf
+    fdd11dbbbbd11ddf
+    cdbbddddddddbbdf
+    cbddddddddddddbc
+    cddddddddddddddc
+    .cccccccccccccc.
+    .fbbfbbbbbbfbbf.
+    ..ff........ff..
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level2`)
+scene.setBackgroundColor(7)
+Farmer = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+EquippedItem = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+EquippedItem.follow(Farmer)
+EquippedItem.follow(Farmer)
+let Hoe = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . b b b b b . . . . . . . . . . 
+    . . . . e . . . . . . . . . . . 
+    . . . . e . . . . . . . . . . . 
+    . . . . e . . . . . . . . . . . 
+    . . . . e . . . . . . . . . . . 
+    . . . . e . . . . . . . . . . . 
+    . . . . e . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.hoe)
+tiles.placeOnTile(Hoe, tiles.getTileLocation(2, 2))
+let SeedBagItem = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 6 . 6 . 6 . 6 . . . . . 
+    . . . . . 6 6 6 6 6 . . . . . . 
+    . . . . . e e e e e . . . . . . 
+    . . . . . 6 6 6 6 6 . . . . . . 
+    . . . . 6 6 6 6 6 6 6 . . . . . 
+    . . . 6 6 6 6 6 6 6 6 6 . . . . 
+    . . . 6 6 6 6 6 6 6 6 6 . . . . 
+    . . . 6 6 6 6 6 6 6 6 6 . . . . 
+    . . . . 6 6 6 6 6 6 6 . . . . . 
+    . . . . . 6 6 6 6 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.seedBag)
+tiles.placeOnTile(SeedBagItem, tiles.getTileLocation(4, 2))
+scene.cameraFollowSprite(Farmer)
+controller.moveSprite(Farmer)
+game.onUpdateInterval(5000, function () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
+    	
+    }
+    for (let value2 of tiles.getTilesByType(assets.tile`myTile0`)) {
+    	
+    }
+})
